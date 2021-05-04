3c489481b498ff1850882f75a46053543305006c
bruno estivalli <bruno.estivalli@gmail.com>
2021-05-04 20:41:45 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/brunoestivalli" target="_blank" rel="noreferrer">Bruno Estivalli</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/bruno_estivalli/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+  </div>
+</footer>
+    );
+}
+
+export default Footer;
